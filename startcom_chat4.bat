96724d8a4cd8afa44ec4e47f98b6d5d1e82f091e
Horia Puscasu <horiapuss@gmail.com>
2025-07-31 23:18:57 +0300

    Add files via upload

--- startcom_chat4.bat
+++ startcom_chat4.bat
@@ -2,12 +2,11 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 :: start cmd /k echo Hello, World!
 @echo off
-nslookup myip.opendns.com resolver1.opendns.com | find "Address" >"%temp%\test1.txt"
-more +1 "%temp%\test1.txt" >"%temp%\test2.txt"
-del %temp%\test1.txt
-set /p MyIP=<%temp%\test2.txt
-del %temp%\test2.txt
-set MyIP=%MyIP:~10%
+FOR /F "tokens=* USEBACKQ" %%F IN (`powershell.exe -noninteractive -command Invoke-RestMethod ipinfo.io/ip`) DO (
+    SET MyIP=%%F
+)
 :: start firefox https://%MyIP%/index_chat.php
-explorer "https://%MyIP%:3129/"
+::explorer "https://%MyIP%:443/"
+
+start "" firefox https://%MyIP%:3129/
 
